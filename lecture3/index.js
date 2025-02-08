5ace8aad9efd079f3a9ac564dda25cf119036ad5
keshavx123 <keshav0760.be21@chitkara.edu.in>
2025-02-08 16:43:26 +0530

    assignment

---
+++ lecture3/index.js
@@ -0,0 +1,30 @@
+// var a=10;
+// let b=20;
+
+
+
+// arr=[1,2,3,4,5,6];
+
+
+// arr.forEach(function(element){
+//       console.log(element);
+// })
+
+
+// for(let i in arr){
+//     console.log(i);
+// }
+
+// for(let i of arr){
+//     console.log(i);
+// }
+
+
+
+// document is a parent of window
+
+
+
+document.getElementById("parafirst"){
+    
+}
