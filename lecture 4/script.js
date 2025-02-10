67331b196e830f95996b0f283a94fcd24d9ff121
keshavx123 <keshav0760.be21@chitkara.edu.in>
2025-02-10 15:39:26 +0530

    lecture 4

---
+++ lecture 4/script.js
@@ -0,0 +1,60 @@
+// javascript is a single threaded and losely typed language
+
+// one thread only--------> process will take place
+// losely typed-> we can add multiple variable type in array
+            // ->variable and calleing from anywhere
+
+
+
+
+// synchronous -> when code run line by line 
+//  asyncronous-> when some code run parallely or wait for their own execution
+
+
+
+// Global Execution context
+// 
+// let,var and cost are hoisted
+
+
+
+// window->parent of document,global object
+// in javascript windows===this(output true)
+// lexical order  or lexical scope -> child calls parent calls grandparent
+
+
+
+
+
+// console.log("hello");
+
+// var a=10;
+
+// function squareofTwoNumber(x){      
+//     var ans =x*x;
+//     return ans;
+// }
+
+// console.log(squareofTwoNumber(a));
+
+
+// console.log(xyz);
+// console.log(sumofTwo(10,16));
+// var xyz=12;
+
+
+// function sumofTwo(a,b){
+//     var ans=a+b;
+//     return ans;
+// }
+
+
+var a=20;
+var b=30;
+var c=50;
+{
+    var a=5;
+    var b=4;
+    var c=43;
+    
+}
