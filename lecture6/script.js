f947a229f87fe385c233afdb604b73d9b7f93a71
keshavx123 <keshav0760.be21@chitkara.edu.in>
2025-02-12 15:35:40 +0530

    lecture 6

---
+++ lecture6/script.js
@@ -0,0 +1,72 @@
+// console.log(1);
+// console.log(sumOfTwo(10,5));
+// function sumOfTwo(a,b){
+//     return a+b;
+// }
+// setTimeout(()=>console.log(4),2000);
+// var start=1;
+// var end=100000000;
+// while(start<end){
+//     start++;
+//     console.log("7");
+// }
+// console.log("end");
+// setInterval(()=>console.log(9),4000)
+
+// first all syncronous task will run and then settimeout and interval(callback) wil run
+
+
+// closure advantage
+// data encapsulation and hiding
+
+
+// closure disadvantage
+
+
+
+
+
+// functions
+
+// function statement
+// function first(){
+//     console.log("first");
+// }
+// first();
+
+
+// //function expression
+
+// const a= function sum(b,c){
+//     return b+c;
+// }
+
+
+
+// // anonymous function
+// const greet = function () {
+//     console.log("Welcome to GeeksforGeeks!");
+// };
+    
+// greet();
+
+
+// // arrow function
+// hello = () => {
+//     return "Hello World!";
+//   }
+
+
+
+
+xyz();
+
+
+function xyz(){
+    console.log("1")
+}
+
+var abc=function cdf(){
+    console.log("2");
+}
+cdf();
